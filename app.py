c1bbfdd20520bbd2e41535bda61bc92f3ac222a9
Old Peanut Team <oldpeanut.ai@gmail.com>
2026-06-08 12:52:57 -0300

    Oops

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ opDesejada = input("Digite a operação que você quer. \n    Sum = Soma\n    Su
 num1 = int(input("Digite o primeiro numero: "))
 num2 = int(input(f"Número escolhido: {num1} Digite o segundo numero: "))
 
-if opDesejada == "sum":
+if opDesejada == "sum" or opDesejada == "Sum":
     print(c.sum(num1, num2))
-elif opDesejada == "sub":
+elif opDesejada == "sub" or opDesejada == "Sub":
     print(c.sub(num1, num2))
 else:
     print("Operação inválida. Use 'Sum' ou 'Sub'.")
