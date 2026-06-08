69d774c49adecbafc24992e168f601a78c533a5a
Old Peanut Team <oldpeanut.ai@gmail.com>
2026-06-07 21:11:09 -0300

    Update (with errors)

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from calc import calc as c
 
+opDesejada = input("Digite a operação que você quer. \n    Sum = Soma\n    Sub = Subtrai \n >> ")
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input(f"Número escolhido: ${num1} Digite o segundo numero: "))
+
+match opDesejada:
+    case "Sum", "sum":
+        resp = c.sum(num1, num2)
+        print(resp)
+    case "Sub", "sub":
+        resp = c.sub(num1, num2)
+        print(resp) 
--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
 class calc:
-    def sum():
-        return "vtmnc"
+    def sum(self, a, b):
+        return(f"O resultado da sua subtração é ${a+b}")
+    
+    def sub(self, a, b):
+        if(a>b):
+            return(f"O resultado da sua subtração é ${a-b}.")
+        else:
+            return(f"A sua subtração vai dar negativo! O resultado é ${a-b}.")
